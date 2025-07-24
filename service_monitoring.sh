97ba48b650d069a0337d82b7513facf8882297c4
samanehghavami <125140059+samanehghavami@users.noreply.github.com>
2025-07-24 19:39:35 +0330

    Update service_monitoring.sh

--- service_monitoring.sh
+++ service_monitoring.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-SERVICES=(nginx,httpd,java)
+SERVICES=(nginx httpd java)
 for service in ${SERVICES[@]}; do
-  if systemctl list-units --all --full | grep service ; then
-    service_name=$(cat /tmp/service | awk '^.' print{$2})
+  if systemctl list-units --all --full | grep $service ; then
+    systemctl status $service > /tmp/service
+    service_name=awk 'NR == 1 print{$2}' /tmp/service
+    status=awk 'NR == 3 print{$2}' /tmp/service
+    memoryusage=awk 'NR == 7 print{$2}' /tmp/service
+    cpuusage=awk 'NR == 8 print{$2}' /tmp/service
+    msg+="service:$service_name is $status cpu usage:$cpuusage memory usage:$memoryusage \n"
+  else
+    echo "service not found"
+  fi
+done
+echo -e $msg
+    
     
